d2c3679fb425ba622104d548845932bdab8a0be1
jhgan77 <>
2026-04-25 08:17:39 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,11 @@
+#test variable
+item_name = "Bread"
+item_price = 4.52
+item_in_stock = 230
+print (item_name, item_price, item_in_stock)
+
+
+# Testing
+print(item_name)
+print(item_price)
+print(item_in_stock)
