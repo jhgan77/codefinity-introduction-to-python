ba01e5e9b6d4498c80fa6d51fc2c1aaf4160d006
jhgan77 <>
2026-04-25 08:28:25 +0000

    Variable calculations

---
+++ variables_and_types/variable_calculations/main.py
@@ -0,0 +1,9 @@
+#calculate soda
+item_name = "Soda"
+print(item_name)
+item_price = 6.99
+purchase_quantity = 5
+total_cost = purchase_quantity*item_price
+
+# Testing
+print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
